e5cfc7d150f958ffb3d426a35e7e10d4e1d3a3bd
anmolsinghal <anmolsinghalpkl@gmail.com>
2022-05-01 19:41:11 -0400

    Adding code for threshold

--- train.cpp
+++ train.cpp
@@ -1,6 +1,28 @@
 #include <vector>
-
+#include <limits>
+#include <list>
+#include <algorithm>
 using namespace std;
+
+
+double sse_loss_at_index(double** A_train, vector<int>& B, int N, int D, int j)
+{
+    
+    double idx_mean = 0.0;
+    double idx_sum = 0.0;
+    for(int i : B) {
+        idx_sum += A_train[j][i];
+    }
+    idx_mean = idx_sum / B.size();
+
+    double accumulate_sse = 0.0; // holds L(j, B) for current j
+    double curr_sse;
+    for(int i : B) {
+        curr_sse = A_train[j][i] - idx_mean;
+        accumulate_sse += curr_sse * curr_sse;
+    }
+    return accumulate_sse;
+}
 
 int sse_loss_per_bucket(double** A_train, vector<int>& B, int N, int D) {
 /*
@@ -11,20 +33,10 @@ int sse_loss_per_bucket(double** A_train, vector<int>& B, int N, int D) {
     double min_sse = 0.0;
     int best_j;
     
-    for(int j = 0; j < D; j++) {
-        double idx_mean = 0.0;
-        double idx_sum = 0.0;
-        for(int i : B) {
-            idx_sum += A_train[j][i];
-        }
-        idx_mean = idx_sum / B.size();
+    for(int j = 0; j < D; j++) {       
 
-        double accumulate_sse = 0.0; // holds L(j, B) for current j
-        double curr_sse;
-        for(int i : B) {
-            curr_sse = A_train[j][i] - idx_mean;
-            accumulate_sse += curr_sse * curr_sse;
-        }
+        double accumulate_sse = sse_loss_at_index(A_train, B, N, D, j);
+       
 
         if(accumulate_sse > min_sse) best_j = j;
     }
@@ -44,20 +56,7 @@ int sse_loss_across_buckets(double** A_train, vector<vector<int>>& B, int N, int
         double accumulate_sse_across_buckets = 0.0;
         
         for(vector<int>& b : B) {
-            double idx_mean = 0.0;
-            double idx_sum = 0.0;
-            for(int i : b) {
-                idx_sum += A_train[j][i];
-            }
-            idx_mean = idx_sum / b.size();
-
-            double accumulate_sse = 0.0; // holds L(j, b) for current j and current b
-            double curr_sse;
-            for(int i : b) {
-                curr_sse = A_train[j][i] - idx_mean;
-                accumulate_sse += curr_sse * curr_sse;
-            }
-
+            double accumulate_sse = sse_loss_at_index(A_train, b, N, D, j);
             accumulate_sse_across_buckets += accumulate_sse;
         }
 
@@ -65,4 +64,77 @@ int sse_loss_across_buckets(double** A_train, vector<vector<int>>& B, int N, int
     }
 
     return best_j;
+}
+
+
+
+double optimal_split_unoptimised(double** A_train, vector<int>& B, int N, int D, int j)
+{   
+
+    auto get_loss = [](list<double> left, list<double> right)
+    {
+        double lmean = 0, rmean = 0;
+
+        for(double l : left)
+        lmean+= l;
+
+        for(double r :right)
+        rmean += r;
+
+        lmean /= left.size();
+        rmean /= right.size();
+
+        double loss = 0;
+
+        for(double l : left)
+        loss += (lmean - l)*(lmean - l);
+
+        for(double r : right)
+        loss += (rmean - r)*(rmean - r);
+
+        return loss;
+    };
+
+    vector<double> column;
+
+    for(int b :B)
+    column.push_back(A_train[b][j]);
+
+    sort(column.begin(), column.end());
+
+    list<double> left;
+    list<double> right;
+    for(int i = 1;i<B.size();i++)
+    right.push_back(column[i]);
+
+    left.push_back(column[0]);
+
+    int best_index = 0;
+
+    double min_loss = get_loss(left, right);
+
+
+    for(int i =1;i< B.size();i++)
+    {
+        double r = right.front();
+        right.pop_front();
+
+        left.push_back(r);
+
+        double loss = get_loss(left, right);
+
+        if(loss < min_loss)
+        {
+            min_loss = loss;
+            best_index = i;
+        }
+
+    }
+
+    if(best_index == column.size()-1)
+    return column[best_index];
+
+    return (column[best_index] + column[best_index+1])/2;
+
+    
 }
