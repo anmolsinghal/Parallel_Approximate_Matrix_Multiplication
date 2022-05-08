7ebcdcd37e4f2c65f33193e9cf408bcc50f253ad
Shubhankar Prasad Ranade <sr6200@access2.cims.nyu.edu>
2022-05-08 18:57:06 -0400

    Temp commit

--- driver.cu
+++ driver.cu
@@ -3,6 +3,13 @@
 #include "train.hpp"
 #include "utils.hpp"
 #include "predict_gpu.cu"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <omp.h>
+
+// nvcc -std=c++11 -Xcompiler -fopenmp driver.cu -o test && ./test
 
 __host__
 void print_matrix(double* mat, int rnum, int cnum) {
@@ -29,126 +36,193 @@ void convert_to_row_major(double* input, double* output, int rows, int cols)
 __host__
 int main(int argc, char** argv) {
     int N = 1000; // number of examples in A_train
-    int D = 128; // size of each example
+    int D = 256; // size of each example
     int C = 8; // number of subspaces
-    int R = 15; // size of each row in B
-    int NUM_RUNS = 100; // number of inference experiments to run
+    int R = 32; // size of each row in B
+    int NUM_RUNS = 5; // number of inference experiments to run
+    
+    int nthreads = 1;
+
+    // handle arguments with getopt
+    char *spec = NULL;
+    int index;
+    int c;
+    char sched_algo;
+
+    opterr = 0;
+
+    while ((c = getopt (argc, argv, "n:d:r:c:")) != -1)
+    switch (c)
+    {
+    case 'n':
+        spec = optarg;
+        sscanf(spec, "%d", &N);
+        break;
+    case 'd':
+        spec = optarg;
+        sscanf(spec, "%d", &D);
+        break;
+    case 'r':
+        spec = optarg;
+        sscanf(spec, "%d", &R);
+        break;
+    case 'c':
+        spec = optarg;
+        sscanf(spec, "%d", &C);
+        break;
+
+    case '?':
+        if (optopt == 's')
+            fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+            fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+            fprintf (stderr,
+                    "Unknown option character `\\x%x'.\n",
+                    optopt);
+        exit(EXIT_FAILURE);
+    
+    default:
+        printf("ERROR opt %c\n", c);
+        exit(EXIT_FAILURE);
+    }
+
+    #pragma omp parallel
+    {
+        #pragma omp sections nowait
+        {
+            #pragma omp section
+            {
+                nthreads = omp_get_num_threads();
+            }
+        }
+    }
+
     double* A_train = generateExamples(N, D);
     double* B = generateExamples(D, R);
+    
+    double* device_matrix, *device_products,  *device_thresholds, *device_output;
+    int *device_indices;
+	cudaMalloc( (void**)&device_products, C*NUM_LEAVES*R* sizeof(double));
+	cudaMalloc( (void**)&device_indices, C*NUM_LEVELS* sizeof(int));
+    cudaMalloc( (void**)&device_thresholds, C*NUM_NODES* sizeof(double));
+
+    printf("Train set size %d, number of subspaces %d\n", N, C);
+    printf("Running %d omp threads\n", nthreads);
 
     Timer timer;
 
     RegressionTree* t = new RegressionTree(D, C);
     
     t->fit(A_train, N);
+    printf("Built regression tree\n");
     // t->print_prototypes();
 
     t->precompute_products(B, R);
     // t->print_products();
 
-    double serial_time = 0.0, omp_time = 0.0;
-
-    int N_test = 1000; // number of examples (rows) in test matrix A
-    double* A_test = generateExamples(N_test, D);
-    
-    double* output_cpu = new double[N_test * R];
-    for(int i =0;i<N_test * R;i++)
-    output_cpu[i] = 0;
-    for(int i = 0; i < NUM_RUNS; i++) {
-        timer.tic();
-        t->predict_cpu(A_test, N_test, output_cpu);
-        omp_time += timer.toc();
-    }
-    printf("Time taken for omp predict %lf\n", omp_time/NUM_RUNS);
-
-
-    double* output = new double[N_test * R];
-    for(int i =0;i<N_test * R;i++)
-    output[i] = 0;
-    for(int i = 0; i < NUM_RUNS; i++) {
-        timer.tic();
-        t->predict(A_test, N_test, output);
-        serial_time += timer.toc();
-    }
-    printf("Time taken for serial predict %lf\n", serial_time/NUM_RUNS);
-
-    printf("Speedup omp over serial: %lf\n", serial_time / omp_time);
-
-    // print predicted output
-    // printf("\nApproximate product:\n{");
-    // print_matrix(output, N_test, R);
-
-    // calculate actual output
-    double* output_ref = new double[N_test * R];
-
-    for(int i = 0; i < R; i++) {
-        for(int j = 0; j < N_test; j++) {
-            output_ref[i*N_test + j] = 0.0;
-            for(int k = 0; k < D; k++) {
-                output_ref[i*N_test + j] += A_test[k*N_test + j] * B[i*D + k];
-            }
-        }
-    }
-
-    // error
-    double max_err = 0;
-    for (long i = 0; i < N_test * R; i++) max_err = max(max_err, fabs(output[i] - output_cpu[i]));
-    printf(" %10e\n", max_err);
-
-
-    cudaEvent_t start,stop;
-    float elapsedTime;
-
-    cudaEventCreate (&start);
-    cudaEventCreate (&stop);
-    double* device_matrix, *device_products,  *device_thresholds, *device_output;
-    int *device_indices;
-	cudaMalloc( (void**)&device_matrix, N_test*D* sizeof(double));
-	cudaMalloc( (void**)&device_products, C*NUM_LEAVES*R* sizeof(double));
-	cudaMalloc( (void**)&device_indices, C*NUM_LEVELS* sizeof(int));
-    cudaMalloc( (void**)&device_thresholds, C*NUM_NODES* sizeof(double));
-    cudaMalloc( (void**)&device_output, N_test*R*sizeof(double));
-    cudaMemset(device_output, 0, N_test*R*sizeof(double));
-
-    double* A_test_row_major = new double[ N_test*D];
-    double* A_test_double_transpose = new double[N_test*D];
-
-    convert_to_row_major(A_test, A_test_row_major, N_test, D);
-    convert_to_row_major(A_test_row_major, A_test_double_transpose, D, N_test);
-    max_err = 0;
-    for (long i = 0; i < N_test * D; i++) max_err = max(max_err, fabs(A_test[i] - A_test_double_transpose[i]));
-    printf("Error from transpose: %10e\n", max_err);
-
-	cudaMemcpy((void*)device_matrix, (void*)A_test_row_major, N_test*D* sizeof(double) ,cudaMemcpyHostToDevice);
 	cudaMemcpy((void*)device_products, (void*)t->products,C*NUM_LEAVES*R *sizeof(double),cudaMemcpyHostToDevice);
     cudaMemcpy((void*)device_indices, (void*)t->indices, C*NUM_LEVELS* sizeof(int),cudaMemcpyHostToDevice);
     cudaMemcpy((void*)device_thresholds, (void*)t->thresholds,C*NUM_NODES* sizeof(double),cudaMemcpyHostToDevice);
-    dim3 dimGrid(N_test);
-    dim3 dimBlock(R);
-	cudaEventRecord (start, 0);
-    omp_time =0.0;
-	for(int i =0;i< NUM_RUNS;i++)
-    {
-        cudaMemset(device_output, 0, N_test*R*sizeof(double));
-        timer.tic();
-        for(int c = 0;c<C;c++)
-        {
-            predict_gpu<<<dimGrid, dimBlock>>>(device_matrix, N_test, D, R, D/C, device_products, device_indices, device_thresholds, device_output, c);
-            cudaDeviceSynchronize();
+
+    double serial_time, omp_time, gpu_time = 0.0;
+
+    // int N_test = 1000; // number of examples (rows) in test matrix A
+    printf("Test matrix size\tSerial Time\tOMP Time\tGPU Time\tOMP Speedup\tGPU Speedup\tOMP Error\tGPU Error\n");
+    // printf("Test matrix size\tOMP Time\tSerial Time\tSpeedup\t\tError\n");
+    for(unsigned long N_test = N; N_test <= 15000; N_test += 1000) {
+        double* A_test = generateExamples(N_test, D);
+        
+        // calculate serial output
+        serial_time = 0.0;
+        double* output = new double[N_test * R];
+        for(int i =0;i<N_test * R;i++)
+        output[i] = 0;
+        for(int i = 0; i < NUM_RUNS; i++) {
+            timer.tic();
+            t->predict(A_test, N_test, output);
+            serial_time += timer.toc();
         }
-        omp_time += timer.toc();
+
+        // calculate omp parallelized output
+        omp_time = 0.0;
+        double* output_cpu = new double[N_test * R];
+        for(int i =0;i<N_test * R;i++)
+        output_cpu[i] = 0;
+        for(int i = 0; i < NUM_RUNS; i++) {
+            timer.tic();
+            t->predict_cpu(A_test, N_test, output_cpu);
+            omp_time += timer.toc();
+        }
+        
+        // absolute error omp vs serial
+        double omp_max_err = 0;
+        for (long i = 0; i < N_test * R; i++)
+            omp_max_err = max(omp_max_err, fabs(output[i] - output_cpu[i]));
+
+        // calculate gpu parallelized output
+        double* A_test_row_major = new double[ N_test*D];
+        // // double* A_test_double_transpose = new double[N_test*D];
+
+        convert_to_row_major(A_test, A_test_row_major, N_test, D);
+        cudaMalloc( (void**)&device_matrix, N_test*D*sizeof(double));
+        cudaMemcpy((void*)device_matrix, (void*)A_test_row_major, N_test*D* sizeof(double) ,cudaMemcpyHostToDevice);
+        // // convert_to_row_major(A_test_row_major, A_test_double_transpose, D, N_test);
+        // // max_err = 0;
+        // // for (long i = 0; i < N_test * D; i++) max_err = max(max_err, fabs(A_test[i] - A_test_double_transpose[i]));
+        // // printf("Error from transpose: %10e\n", max_err);
+        
+        cudaMalloc( (void**)&device_output, N_test*R*sizeof(double));
+        // cudaMemset(device_output, 0, N_test*R*sizeof(double));
+        cudaEvent_t start,stop;
+        float elapsedTime;
+        cudaEventCreate (&start);
+        cudaEventCreate (&stop);
+
+        dim3 dimGrid(N_test);
+        dim3 dimBlock(R);
+        cudaEventRecord (start, 0);
+        gpu_time = 0.0;
+        for(int i = 0; i < NUM_RUNS; i++) {
+            cudaMemset(device_output, 0, N_test*R*sizeof(double));
+        //     // double starttime = omp_get_wtime();
+            timer.tic();
+            for(int c = 0; c < C; c++) {
+                predict_gpu<<<dimGrid, dimBlock>>>(device_matrix, N_test, D, R, D/C, device_products, device_indices, device_thresholds, device_output, c);
+                cudaDeviceSynchronize();
+            }
+        //     // gpu_time += omp_get_wtime() - starttime;
+            gpu_time += timer.toc();
+        }
+
+        double* host_result = new double[N_test*R];
+        // double* host_result_col_major = new double[N_test*R];
+        cudaMemcpy((void*)host_result, (void*)device_output,N_test*R*sizeof(double),cudaMemcpyDeviceToHost);
+        // convert_to_row_major(host_result, host_result_col_major, N_test, R);
+
+        cudaEventRecord (stop, 0);
+        cudaEventSynchronize (stop);
+        cudaEventElapsedTime ( &elapsedTime, start, stop);
+
+        double gpu_max_error = 0;
+        for (long i = 0; i < N_test * R; i++)
+            gpu_max_error = max(gpu_max_error, fabs(host_result[i] - output[i]));
+
+        delete [] output;
+        delete [] output_cpu;
+        delete [] A_test_row_major;
+        delete [] host_result;
+        cudaFree(device_output);
+
+        //print all
+        printf("\n%lu x %d\t      ", N_test, D);
+        printf("%10lf\t", serial_time/NUM_RUNS);
+        printf("%10lf\t", omp_time/NUM_RUNS);
+        printf("%10lf\t", gpu_time/NUM_RUNS);
+        printf("%10lf\t", serial_time / omp_time);
+        printf("%10lf\t", serial_time / gpu_time);
+        printf("%10e\t", omp_max_err);
+        printf("%10e", gpu_max_error);
     }
-
-	cudaEventRecord (stop, 0);
-	cudaEventSynchronize (stop);
-	cudaEventElapsedTime ( &elapsedTime, start, stop);
-
-    double* host_result = new double[N_test*R];
-    cudaMemcpy((void*)host_result, (void*)device_output,N_test*R*sizeof(double),cudaMemcpyDeviceToHost);
-
-    max_err = 0;
-    for (long i = 0; i < N_test * R; i++) max_err = max(max_err, fabs(host_result[i] - output_cpu[i]));
-    printf("Error from GPU: %10e\n", max_err);
-    printf("Time taken for gpu predict %lf\n", omp_time/NUM_RUNS);
-
+    
 }
--- regressionTree_old.hpp
+++ regressionTree_old.hpp
@@ -2,10 +2,12 @@
 #define REGRESSIONTREE_HPP
 #include "train.hpp"
 
-#include <omp.h>
+// #include <unistd.h>
 #include <stdio.h>
+#include <algorithm>
 #include <vector>
 #include <cmath>
+#include <unordered_map>
 using namespace std;
 
 #define NUM_LEVELS 4
@@ -16,6 +18,9 @@ using namespace std;
 class RegressionTree
 {
 public:
+    // double thresholds[NUM_NODES];
+    // int indices[NUM_LEVELS];
+    // double prototypes[NUM_LEAVES][PROTOTYPE_DIM];
     double* thresholds;
     int* indices;
     double* prototypes;
@@ -49,11 +54,11 @@ public:
             cur_level.push_back(root);
             
             for(int i = 0; i < NUM_LEVELS; i++) {
-                // printf("\nAt subspace %d, level %d, cur_level size is %lu\n", c, i, cur_level.size());
+                //printf("\nAt subspace %d, level %d, cur_level size is %lu\n", c, i, cur_level.size());
                 vector<vector<int>> next_level;
 
                 int j = heuristic_select_split_idx(A_train, cur_level, N, M, c * M);
-                // printf("found optimal split %d\n", j);
+                //printf("found optimal split %d\n", j);
 
                 indices[c*NUM_LEVELS + i] = j;
 
@@ -61,9 +66,8 @@ public:
                 {   
                     auto b = cur_level[k];
                     double threshold =  optimal_split_unoptimised(A_train, N, b, j);
-                    // printf("found optimal threshold for node %d\n", (int)pow(2,i)-1+k);
-                    thresholds[ c*NUM_NODES + (int)pow(2,i)-1+k ] = threshold;
-
+                    //printf("found optimal threshold for node %d\n", (int)pow(2,i)-1+k);
+                    thresholds[ c*NUM_NODES + (int)pow(2,i)-1+k ] = threshold; 
                     vector<int> left;
                     vector<int> right;
 
@@ -82,7 +86,7 @@ public:
                 cur_level = next_level;
 
             } // built all levels for current subspace c
-            // printf("built all levels for subspace %d, final cur level size is %lu\n", c, cur_level.size());
+            //printf("built all levels for current subspace, final cur level size is %lu\n", cur_level.size());
 
             // calculate prototypes for c
             // at this stage, cur_level.size() == NUM_LEAVES
@@ -148,6 +152,27 @@ public:
     }
     
     //dimension of B is D*R
+    // void precompute_products(double* B, int R)
+    // {   
+    //     this->R = R;
+    //     products = new double[C * NUM_LEAVES * R];
+    //     // initialize products array
+    //     for(int i = 0; i < C * NUM_LEAVES * R; i++) products[i] = 0.0;
+
+    //     for(int i = 0; i < C; i++)
+    //     {   
+    //         for(int j = 0; j < R; j++)
+    //         {
+    //             for(int k = 0; k < NUM_LEAVES; k++)
+    //             {
+    //                 double product = dot_product(prototypes + (i*NUM_LEAVES+k)*M, B + j*D + i*M, M); 
+    //                 //set value of product at ith subspace, kth leaf, and jth col
+    //                 products[i*NUM_LEAVES*R + k*R + j ] += product;
+    //             }
+    //         }
+    //     }
+    // }
+
     void precompute_products(double* B, int R)
     {   
         this->R = R;
@@ -164,7 +189,7 @@ public:
                     for(int i = 0; i < NUM_LEAVES; i++)
                     {
                         // double product = dot_product(prototypes + (c*NUM_LEAVES+i)*M, B + j*D + c*M, M);
-                        // //set value of product at ith subspace, kth leaf, and jth col
+                        // set value of product at ith subspace, kth leaf, and jth col
                         // products[c*NUM_LEAVES*R + i*R + j ] += product;
                         products[c*NUM_LEAVES*R + j*NUM_LEAVES + i] += prototypes[c*NUM_LEAVES*M + k*NUM_LEAVES + i] * B[c*M + j*D + k]; // col major products
                     }
@@ -173,12 +198,26 @@ public:
         }
     }
     
+    // int get_leaf_idx(double* in, int row, int c, int N)
+    // {
+    //     int cur_index = 0;
+        
+    //     for(int i =0;i< NUM_LEVELS-1;i++)
+    //     {
+    //         int b = in[indices[c*NUM_LEVELS + i] * N + row] >= thresholds[c*NUM_NODES + cur_index];
+
+    //         cur_index = 2*cur_index + 1 + b;
+    //     }
+
+    //     return cur_index;
+    // }
+
     int get_leaf_idx(double* in, int row, int c, int N)
     {
         int cur_index = 0;
         
         for(int i = 0; i < NUM_LEVELS-1; i++) {
-            int b = in[indices[i] * N + row] >= thresholds[c*NUM_NODES + cur_index];
+            int b = in[indices[c*NUM_LEVELS + i] * N + row] >= thresholds[c*NUM_NODES + cur_index];
             cur_index = 2*cur_index + 1 + b;
         }
 
@@ -186,6 +225,23 @@ public:
     }
 
     //Dimension of input is N*D, output is N*R
+    // void predict(double* input, int N, double* output)
+    // {
+    //     for(int i =0;i<N;i++)
+    //     {
+    //         for(int j =0;j<R;j++)
+    //         {
+    //             output[j * N + i] = 0.0;
+    //             for(int k= 0;k<C;k++)
+    //             {
+    //                 int leaf = get_leaf_idx(input, i, k, N);
+    //                 double product = products[k*NUM_LEAVES*R + leaf*R + j];
+    //                 output[j * N + i] += product;
+    //             }
+    //         }
+    //     }
+    // }
+
     void predict(double* input, int N, double* output) {        
         for(int j = 0; j < R; j++) {
             for(int c = 0; c < C; c++) {
@@ -200,6 +256,27 @@ public:
         }
     }
 
+    // void predict_cpu(double* input, int N, double* output)
+    // {   
+    //     double out;
+	// #pragma omp parallel for collapse(2)
+    //     for(int i =0;i<N;i++)
+    //     {
+    //         for(int j =0;j<R;j++)
+    //         {
+    //             out = 0;
+    //             #pragma omp for reduction(+:out)
+    //             for(int k= 0;k<C;k++)
+    //             {
+    //                 int leaf = get_leaf_idx(input, i, k, N);
+    //                 double product = products[k*NUM_LEAVES*R + leaf*R + j];
+    //                 out += product;
+    //             }
+
+    //             output[j * N + i] = out;
+    //         }
+    //     }
+    // }
     void predict_cpu(double* input, int N, double* output)
     {
         #pragma omp parallel
